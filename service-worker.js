cb13d6d044251118797f979e1015f72d8bfff08e
Yinon Avraham <yinonavraham@gmail.com>
2024-12-18 22:10:43 +0200

    Comment out non existing files from caching

--- service-worker.js
+++ service-worker.js
@@ -2,12 +2,12 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('my-pwa-cache').then(cache => {
       return cache.addAll([
-        '/pwa-github-pages/',
+        // '/pwa-github-pages/',
         '/pwa-github-pages/index.html',
-        '/pwa-github-pages/styles.css',
-        '/pwa-github-pages/app.js',
+        // '/pwa-github-pages/styles.css',
+        // '/pwa-github-pages/app.js',
         '/pwa-github-pages/images/icon-192x192.png',
-        '/pwa-github-pages/images/icon-512x512.png',
+        // '/pwa-github-pages/images/icon-512x512.png',
       ]);
     })
   );
